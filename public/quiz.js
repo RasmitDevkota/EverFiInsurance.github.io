f5a3be3b9d4beb6e618d6e5c46a76793772e8d19
Dr AlienTech <rdatch101@gmail.com>
2020-01-20 13:40:32 -0500

    images, js

---
+++ public/quiz.js
@@ -0,0 +1,20 @@
+function start() {
+
+};
+
+function next(q) {
+    $('#q' + q).hide();
+    $('#next' + q + 1).show();
+};
+
+function checkbox(q, correct) {
+    $('#correct' + q).hide();
+    $('#wrong' + q).hide();
+    if ($('#q' + q + correct).is(':checked')) {
+        $('#correct' + q).show();
+        $('#next' + q).show();
+    } else {
+        $('#wrong' + q).show();
+        $('#next' + q).hide();
+    }
+};
